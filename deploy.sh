1750cd69756fb51470d8f15e1208f397fa9848d5
Bucky0719 <100702108+Bucky0719@users.noreply.github.com>
2024-04-10 12:20:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
 
 if [[ $GIT_BRANCH == "origin/main" ]]; then
-    chmod +x build.sh
-    ./build.sh
+    sh "chmod +x build.sh"
+    sh "./build.sh"
     docker tag php-image bucky0838/testing
     docker push bucky0838/testing
 
